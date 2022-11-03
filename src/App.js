906fd988fee1a41d955c62d5668e9b899566f8dc
Pawel Zarzycki <pawel.zarzycki@capgemini.com>
2022-11-03 14:25:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import {lazy} from "react";
+
+const NavTestComponent = lazy(() => import('nav/Components.js'))
+const NavTestComponentTwo = lazy(() => import('nav/ComponentTwo.js'))
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavTestComponent />
+        <NavTestComponentTwo/>
+    main content
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require('webpack').container;
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const stylesHandler = "style-loader";
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+      library: 'MyLibrary',
+      libraryTarget: 'umd'
+  },
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: '3000'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: `
+    <html>
+      <body>
+        <div id="root"></div>
+      </body>
+    </html>
+  `
+    }),
+    new ModuleFederationPlugin({
+      name: 'main',
+      filename: 'remoteEntry.js',
+      remotes: { nav:'nav@http://localhost:3001/remoteEntry.js'},
+      exposes: {}
+    }),
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader"],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: "asset",
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  optimization: {
+    sideEffects: false,
+    moduleIds: 'named',
+    chunkIds: 'named'
+  }
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+  }
+  return config;
+};
